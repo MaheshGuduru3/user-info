7bbf4dcc3cc63349b6d2e7f0bd2931e87ff25b9d
MaheshGuduru3 <maheshguduru30@gmail.com>
2024-02-11 14:54:20 +0530

    removed .env1

--- user-form/src/components/Home.js
+++ user-form/src/components/Home.js
@@ -9,15 +9,20 @@ const Home = () => {
   return (
     <div className='container-fluid border b-2'>
        <div className="container-fluid  d-flex flex-column justify-content-center align-items-center" style={{height:'100vh'}}> 
-  {localStorage.getItem('user') ? 
+        {        localStorage.getItem('user') ? 
+            <>
            <h1>Hello , Welcome</h1>
            <h5>email : {JSON.parse(localStorage.getItem('user')).email}</h5>
            <h5>name : {JSON.parse(localStorage.getItem('user')).username}</h5>
            <button className='btn btn-primary p-2' onClick={()=> {window.localStorage.removeItem('user'); navigate('/login')}}>Logout</button>
-        </div> :
-    <NavLink to='/login' className='text-primary fs-3'>Login</NavLink>
-    <NavLink to='/register' className='text-primary fs-3'>Register</NavLink>
+           </>
+           :
+           <>
+           <NavLink to='/login' className='text-primary fs-3'>Login</NavLink>
+           <NavLink to='/register' className='text-primary fs-3'>Register</NavLink>
+           </>
   }
+   </div>
     </div>
   )
 }
