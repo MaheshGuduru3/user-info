92d6efca1e6f840be8cb29d7cd5e14c0598cf496
MaheshBabuGuduru <136345745+MaheshGuduru3@users.noreply.github.com>
2024-02-11 10:50:57 +0530

    Update Home.js

--- user-form/src/components/Home.js
+++ user-form/src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate ,   NavLink } from 'react-router-dom'
 
 
 const Home = () => {
@@ -9,11 +9,15 @@ const Home = () => {
   return (
     <div className='container-fluid border b-2'>
        <div className="container-fluid  d-flex flex-column justify-content-center align-items-center" style={{height:'100vh'}}> 
+  {localStorage.getItem('user') ? 
            <h1>Hello , Welcome</h1>
            <h5>email : {JSON.parse(localStorage.getItem('user')).email}</h5>
            <h5>name : {JSON.parse(localStorage.getItem('user')).username}</h5>
            <button className='btn btn-primary p-2' onClick={()=> {window.localStorage.removeItem('user'); navigate('/login')}}>Logout</button>
-        </div>
+        </div> :
+    <NavLink to='/login' className='text-primary fs-3'>Login</NavLink>
+    <NavLink to='/register' className='text-primary fs-3'>Register</NavLink>
+  }
     </div>
   )
 }
